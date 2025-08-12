a4b48184203632adab9308a5c755a2bd5ecbaf1f
ramumaheshwaram <maheshwaramramu16@gmail.com>
2025-08-12 08:54:35 +0530

    Using of select button to add the cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -23,7 +23,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-quantity-container">
-          <select>
+          <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -66,20 +66,27 @@ document.querySelectorAll('.js-add-to-cart')
           matchingItem = item;
         }
       });
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+      const quantity = Number(quantitySelector.value);
+
+
 
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
       }
+
       else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: quantity
         });
       }
       let cartlQuantity = 0;
       cart.forEach((item) => {
         cartlQuantity += item.quantity;
       });
+      console.log(cart);
+
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartlQuantity;
     });

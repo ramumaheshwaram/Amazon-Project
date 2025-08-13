16b4c136c35c79b82907a37b23213b806b436c5a
ramumaheshwaram <maheshwaramramu16@gmail.com>
2025-08-13 19:32:46 +0530

    Lesson 14 Exersices solutions and  i am getting error using grok to solve that error fix the all the code

--- data/cart.js
+++ data/cart.js
@@ -1,64 +1,55 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
-
-if (!cart) {
-  cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
+  {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
-  }, {
+  },
+  {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
   }
-  ];
-}
+];
 
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-
-export function addToCart(productId) {
+export function addToCart(productId, quantity) {
   let matchingItem;
-
   cart.forEach((cartItem) => {
     if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
   });
 
-  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-  const quantity = Number(quantitySelector.value);
-
-
-
   if (matchingItem) {
     matchingItem.quantity += quantity;
-  }
-
-  else {
+  } else {
     cart.push({
       productId,
       quantity
     });
   }
   saveToStorage();
-
 }
 
 export function removeCart(productId) {
-  const newCart = [];
-  cart.forEach((cartItem) => {
-    if (cartItem.productId !== productId) {
-      newCart.push(cartItem);
-    }
-  });
-  cart = newCart
+  cart = cart.filter((cartItem) => cartItem.productId !== productId);
   saveToStorage();
 }
 
 export function calculateCartQuantity() {
-  let cartQuantity = 0;
+  return cart.reduce((total, cartItem) => total + cartItem.quantity, 0);
+}
+
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
   cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
   });
-  return cartQuantity;
+  if (matchingItem) {
+    matchingItem.quantity = newQuantity;
+    saveToStorage();
+  }
 }
